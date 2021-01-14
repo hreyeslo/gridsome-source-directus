748d9f5f586e2c9e9771ac96813c93ea4d861560
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2021-01-14 10:02:06 +0100

    Fixed for directus v9

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ const imageTypes = [
 
 // TODO ADD CLEANUP OF UNUSED IMAGES / FILES
 let download = async (url, dest, dir = uploadImagesDir) => {
-  
+
   var imgName = dest;
 
-  if (!fs.existsSync('./.cache-directus')){
+  if (!fs.existsSync('./.cache-directus')) {
     fs.mkdirSync('./.cache-directus');
   }
 
-  if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
 
   dest = dir + '/' + dest;
@@ -41,21 +41,21 @@ let download = async (url, dest, dir = uploadImagesDir) => {
 
   return new Promise((resolve, reject) => {
     var file = fs.createWriteStream(dest);
-    http.get(url, function(response) {
+    http.get(url, function (response) {
       response.pipe(file);
-      file.on('finish', function() {
+      file.on('finish', function () {
         resolve(cleanImageName);
       });
-    }).on('error', function(err) {
+    }).on('error', function (err) {
       fs.unlink(dest);
       reject(err.message);
     });
-  });  
+  });
 };
 
 function sanitizeFields(fields) {
-  Object.keys(fields).forEach((key) => { 
-    if(fields[key] === null || fields[key] === undefined) {
+  Object.keys(fields).forEach((key) => {
+    if (fields[key] === null || fields[key] === undefined) {
       delete fields[key];
     }
   });
@@ -65,10 +65,10 @@ function sanitizeFields(fields) {
 function sanitizeItem(fields) {
   let { id, title, slug, path, date, content, excerpt } = fields;
 
-  let _id = id.toString();
-
-  delete fields.id;
-  fields._directusID = _id;
+  if (id) {
+    delete fields.id;
+    fields._directusID = id;
+  }
 
   return sanitizeFields(fields);
 }
@@ -108,11 +108,11 @@ function flatten(obj) {
 
 async function checkForImages(item) {
 
-  for(const itemKey in item) {
+  for (const itemKey in item) {
     const itemContent = item[itemKey];
-    if(itemContent && itemContent.type && imageTypes.includes(itemContent.type)) {
+    if (itemContent && itemContent.type && imageTypes.includes(itemContent.type)) {
       item[itemKey].gridsome_image = await download(itemContent.data.full_url, itemContent.filename_disk);
-    } else if(itemContent && itemKey !== 'owner' && typeof itemContent === 'object' && Object.keys(itemContent).length > 0) {
+    } else if (itemContent && itemKey !== 'owner' && typeof itemContent === 'object' && Object.keys(itemContent).length > 0) {
       item[itemKey] = await checkForImages(itemContent);
     }
   }
@@ -122,11 +122,11 @@ async function checkForImages(item) {
 
 async function checkForDownloads(item) {
 
-  for(const itemKey in item) {
+  for (const itemKey in item) {
     const itemContent = item[itemKey];
-    if(itemContent && itemContent.type && itemContent.data) {
+    if (itemContent && itemContent.type && itemContent.data) {
       item[itemKey].gridsome_link = await download(itemContent.data.full_url, itemContent.filename_disk, './.cache-directus/file-cache');
-    } else if(itemContent && itemKey !== 'owner' && typeof itemContent === 'object' && Object.keys(itemContent).length > 0) {
+    } else if (itemContent && itemKey !== 'owner' && typeof itemContent === 'object' && Object.keys(itemContent).length > 0) {
       item[itemKey] = await checkForDownloads(itemContent);
     }
   }
@@ -135,11 +135,11 @@ async function checkForDownloads(item) {
 }
 
 class DirectusSource {
-  static defaultOptions () {
+  static defaultOptions() {
     return {
       typeName: 'Directus',
       apiUrl: undefined,
-      project: '_',
+      project: '',
       staticToken: undefined,
       email: undefined,
       password: undefined,
@@ -149,79 +149,72 @@ class DirectusSource {
     }
   }
 
-  constructor (api, options) {
+  constructor(api, options) {
     this.api = api;
     this.options = options;
 
     /**
      * Options for setting download protocol && images upload directory
      */
-    if( options.global ) {
-      if( options.global.protocol ) {
+    if (options.global) {
+      if (options.global.protocol) {
         http = require(options.global.protocol)
       }
-      if( options.global.uploadImagesDir ) {
+      if (options.global.uploadImagesDir) {
         uploadImagesDir = options.global.uploadImagesDir
       }
     }
     api.loadSource(args => this.fetchContent(args));
   }
 
-  async fetchContent (store) {
+  async fetchContent(store) {
     const { addCollection, getContentType, slugify } = store
     const { apiUrl, project, staticToken, email, password, collections, maxRetries, reconnectTimeout } = this.options
 
-    const directusOptions = {
-      url: apiUrl,
-      project: project,
-      token: staticToken
-    };
-    
-    const client = new DirectusSDK(directusOptions);
+    const client = new DirectusSDK(apiUrl);
 
     let retries = 0;
 
     let connect = async () => {
       return new Promise(async (resolve, reject) => {
-          try {
-            await client.login(Object.assign({ email, password, persist: false }, directusOptions));
-            
-            resolve(await client.getCollections());
-          } catch (e) {
-            console.error("DIRECTUS ERROR: Can not login to Directus", e);
-  
-            if(retries < maxRetries) {
-              retries++;
-              console.log("DIRECTUS - Retrying to connect in 10 seconds...");
-  
-              setTimeout(async () => {
-                await connect();
-              }, reconnectTimeout);
-            } else {
-              reject(process.exit(1))
-              throw new Error("DIRECTUS ERROR: Can not login to Directus");
-            }          
+        try {
+          await client.auth.login(Object.assign({ email, password }));
+          resolve(await client.collections.read());
+        } catch (e) {
+          console.error("DIRECTUS ERROR: Can not login to Directus", e);
+
+          if (retries < maxRetries) {
+            retries++;
+            console.log("DIRECTUS - Retrying to connect in 10 seconds...");
+
+            setTimeout(async () => {
+              await connect();
+            }, reconnectTimeout);
+          } else {
+            reject(process.exit(1))
+            throw new Error("DIRECTUS ERROR: Can not login to Directus");
           }
-      });      
-    }    
-    
-    if(email && password) {
-      let data = await connect();  
+        }
+      });
     }
-    
+
+    if (email && password || staticToken) {
+      let data = await connect();
+    }
+
     console.log("DIRECTUS: Loading data from Directus at: " + apiUrl);
 
-    if(collections.length <= 0) {
+    if (collections.length <= 0) {
       console.error("DIRECTUS ERROR: No Directus collections specified!");
       process.exit(1)
       throw new Error("DIRECTUS ERROR: No Directus collections specified!");
     }
 
-    for(const collection of collections) {
+    for (const collection of collections) {
       let collectionName;
       let params;
       let directusPathName;
-      if(typeof collection === 'object') {
+      if (typeof collection === 'object') {
         collectionName = collection.name;
         directusPathName = collection.directusPathName || collectionName
         delete collection.name;
@@ -229,55 +222,55 @@ class DirectusSource {
       } else {
         collectionName = collection;
       }
-      
+
       try {
-        if(!params.limit) {
+        if (!params.limit) {
           params.limit = -1;
         }
 
-        let data = await client.getItems(directusPathName, params);
+        let data = await client.items(directusPathName).read(params);
         data = data.data;
 
         let route;
 
-        if(params) {
-          if(params.hasRoute) {
+        if (params) {
+          if (params.hasRoute) {
             route = `/${slugify(collectionName)}/:slug`;
-          } else if(params.route) {
-            if(typeof params.route === 'function') {
+          } else if (params.route) {
+            if (typeof params.route === 'function') {
               route = params.route(collectionName, collection, slugify);
             } else {
               route = params.route;
             }
           }
         }
-        
+
         const contentType = addCollection({
           typeName: collectionName, // TODO change name creation
           route: route
         })
-        
-        for(let item of data) {
 
-          if(params.downloadImages) {
+        for (let item of data) {
+
+          if (params.downloadImages) {
             item = await checkForImages(item);
           }
 
-          if(params.downloadFiles) {
+          if (params.downloadFiles) {
             item = await checkForDownloads(item);
           }
 
           /**
            * Convert nested object to flat object
            */
-          if( params.flat ) {
+          if (params.flat) {
             item = flatten(item);
           }
 
           /**
            * Check if params.sanitizeID === false to sanitize Node ID or not
            */
-          if( params.sanitizeID === false ) {
+          if (params.sanitizeID === false) {
             contentType.addNode(sanitizeFields(item))
           }
           else {
@@ -293,7 +286,7 @@ class DirectusSource {
     }
 
     console.log("DIRECTUS: Loading done!");
-    client.logout();
+    client.auth.logout();
 
   }
 }
